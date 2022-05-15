61b7742b64b1f4378ebbb233f7bd067d675b333a
atliibekir <atliibekir@gmail.com>
2022-05-15 22:34:35 +0300

    15Mayıs

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,3 +1,5 @@
+import { ProductService } from './../../services/product.service';
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  constructor() {}
+  dataLoaded =false;
+  constructor(private productService:ProductService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded=true;
+    });
+  }
 }
